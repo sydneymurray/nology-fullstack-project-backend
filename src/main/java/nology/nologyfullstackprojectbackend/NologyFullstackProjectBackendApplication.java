f04f7602d80f0b97081449dfebce6a1467ecd074
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-06-01 20:56:23 +0100

    Initial Commit

---
+++ src/main/java/nology/nologyfullstackprojectbackend/NologyFullstackProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package nology.nologyfullstackprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NologyFullstackProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NologyFullstackProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nology/nologyfullstackprojectbackend/NologyFullstackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nology.nologyfullstackprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NologyFullstackProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
