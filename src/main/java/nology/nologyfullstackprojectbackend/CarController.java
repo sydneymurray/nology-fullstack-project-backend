b0f52b17bc7951e1374ae54ca7a26b821922a387
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-06-05 18:50:07 +0100

    errors

--- src/main/java/nology/nologyfullstackprojectbackend/CarController.java
+++ src/main/java/nology/nologyfullstackprojectbackend/CarController.java
@@ -10,14 +10,14 @@ import java.util.List;
 public class CarController {
     @Autowired
     CarRepository carRepository;
+
     @GetMapping("/car")
     public String getCar(){
         return "Hello Everyone, still working";
     }
-/*
+
     @GetMapping("/cars")
     public List<Car> getCars(){
         return carRepository.findAll();
     }
-*/
 }
