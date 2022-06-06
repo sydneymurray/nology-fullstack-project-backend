e19eb080a3cf17a64c01660dfeb24b8cdcd5ed9b
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-06-06 20:33:04 +0100

    Car CRUD completed

--- src/main/java/nology/nologyfullstackprojectbackend/CarController.java
+++ src/main/java/nology/nologyfullstackprojectbackend/CarController.java
@@ -10,15 +10,16 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CarController {
     @Autowired
     CarRepository carRepository;
 
-    @GetMapping("/car")
-    public String getCar(){
-        return "Hello Everyone, still working";
+    @GetMapping("/car/{carId}")
+    public Optional<Car> getCar(@PathVariable int carId){
+        return this.carRepository.findById(carId);
     }
 
     @PostMapping("/car")
@@ -41,4 +42,26 @@ public class CarController {
         if (deleted > 0) return "Car was deleted okay";
         return "Car Id: " + carId +" does not exist";
     }
+
+    @PutMapping("/car/{carId}")
+    public String updateCar(@PathVariable int carId, @RequestBody Car carInfo){
+        Optional<Car> existingCar = carRepository.findById(carId);
+        if (existingCar.isEmpty()) return "Car: " + carId + " Does not exist.";
+        this.carRepository.save(carInfo);
+        return "Car " + carInfo.getModel() + " saved okay";
+    }
 }
+
+/*
+    @Override
+    @Transactional(propagation= Propagation.REQUIRES_NEW)
+    public UserControl getUserControlById(Integer id){
+        return this.userControlRepository.getOne(id);
+    }
+
+
+    @GetMapping("/car")
+    public String getCar(){
+        return "Hello Everyone, still working";
+    }
+ */
